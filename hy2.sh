62c352e9ccb9a7ee8a086af877d1ae39598d72c0
ziseyu <qq1426357211@gmail.com>
2026-05-04 15:54:53 +0800

    Change default AUTH_PASSWORD in hy2.sh
    Updated the authentication password in the script.

--- hy2.sh
+++ hy2.sh
@@ -8,7 +8,7 @@ set -e
 # ---------- 默认配置 ----------
 HYSTERIA_VERSION="v2.6.5"
 DEFAULT_PORT=22222         # 自适应端口
-AUTH_PASSWORD="ieshare2025"   # 建议修改为复杂密码
+AUTH_PASSWORD="ziseyu"   # 建议修改为复杂密码
 CERT_FILE="cert.pem"
 KEY_FILE="key.pem"
 SNI="www.bing.com"
